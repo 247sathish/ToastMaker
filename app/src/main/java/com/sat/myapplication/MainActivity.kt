073b75a1bfc9f62c099f4cb7a76847abaecafb9d
SathishkumarChandrababu <Sathishkumar.chandrababu@kalpitatechnologies.com>
2022-06-01 15:25:45 +0530

    Reay for library

--- app/src/main/java/com/sat/myapplication/MainActivity.kt
+++ app/src/main/java/com/sat/myapplication/MainActivity.kt
@@ -2,12 +2,12 @@ package com.sat.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sat.toastmaker.ToastMake
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ToastMake.SToast(applicationContext,"Hiiiiiiiiiiiiiiiiiiii i am from library")
+        //ToastMake.SToast(applicationContext,"Hiiiiiiiiiiiiiiiiiiii i am from library")
     }
 }
